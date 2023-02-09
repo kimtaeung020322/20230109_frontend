ec2426e345f379bb39952e1de70b1ab3e9d03dcd
hbs30832 <hbs9312@gmail.com>
2023-02-09 16:19:37 +0900

    로그인 기능 구현

--- neppstagram/src/component/page/SignUp.jsx
+++ neppstagram/src/component/page/SignUp.jsx
@@ -1,10 +1,20 @@
+import styled from "styled-components";
+import SignUpForm from "../login/SignUpForm";
+
 // src/component/page/SignUp.jsx
 function SignUp() {
   return (
-    <div>
-      <h1>SignUp</h1>
-    </div>
+    <Container>
+      <SignUpForm />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 export default SignUp;
