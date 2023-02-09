c3f02281f7ee4095f90175ff60de852b0ded03e8
hbs30832 <hbs9312@gmail.com>
2023-02-09 12:51:06 +0900

    Input 수정

--- neppstagram/src/App.js
+++ neppstagram/src/App.js
@@ -1,14 +1,23 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import Header from "./component/common/Header";
 import { Router } from "./router";
 import { theme } from "./styles";
 
 function App() {
+  // App이 마운트 되자마자 login 페이지 이동
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("login");
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <Header />
-        <Router />
+        <Wrapper>
+          <Router />
+        </Wrapper>
       </Container>
     </ThemeProvider>
   );
@@ -16,6 +25,12 @@ function App() {
 
 const Container = styled.div`
   background: #eee;
+  min-height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
 `;
 
 export default App;
--- neppstagram/src/component/common/Header.jsx
+++ neppstagram/src/component/common/Header.jsx
@@ -1,10 +1,44 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { AiOutlineSearch, AiOutlineUser, AiOutlineHome } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import Input from "./Input";
 
 function Header() {
+  const [input, setInput] = useState("");
   return (
     <Container>
       <Wrapper>
         <h1>Neppstagram</h1>
+        <Input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder={
+            <>
+              <AiOutlineSearch />
+              검색
+            </>
+          }
+        />
+        <Gnb>
+          <GnbList>
+            <li>
+              <Link to="home">
+                <AiOutlineHome />
+              </Link>
+            </li>
+            <li>
+              <Link to="search">
+                <AiOutlineSearch />
+              </Link>
+            </li>
+            <li>
+              <Link to="profile">
+                <AiOutlineUser />
+              </Link>
+            </li>
+          </GnbList>
+        </Gnb>
       </Wrapper>
     </Container>
   );
@@ -17,8 +51,18 @@ const Container = styled.header`
 `;
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
   max-width: 500px;
   margin: 0 auto;
+`;
+
+const Gnb = styled.nav``;
+
+const GnbList = styled.ul`
+  display: flex;
+  gap: 10px;
 `;
 
 export default Header;
